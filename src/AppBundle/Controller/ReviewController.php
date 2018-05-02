8060617bb2b7106af358ac7b8a92e3fd09e28c8d
Benoite Lallemand <benlall@hotmail.fr>
2018-05-02 21:52:00 +0200

    review forms

--- src/AppBundle/Controller/ReviewController.php
+++ src/AppBundle/Controller/ReviewController.php
@@ -8,7 +8,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Request;
 
+
 use AppBundle\Entity\Review;
+use AppBundle\Form\ReviewType;
 
 /**
  * Review controller
@@ -27,7 +29,13 @@ class ReviewController extends Controller
 
     public function indexAction()
     {
-        return $this->render('review/index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+        $reviews = $em->getRepository('AppBundle:Review')->findAll();
+
+        return $this->render('review/index.html.twig', array(
+            'reviews' => $reviews,
+        ));
     }
 
     /**
@@ -39,7 +47,102 @@ class ReviewController extends Controller
 
     public function newAction(Request $request)
     {
-        return $this->render('review/new.html.twig');
+        $review = new Review();
+        //$form = $this->createForm(ReviewType::class, $review);
+        $form = $this->createForm('AppBundle\Form\ReviewType', $review);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($review);
+            $em->flush();
+
+            // You can use too :
+            // return $this->redirect($this->generateUrl('review_show', array('id' => $review->getId())))
+
+            return $this->redirectToRoute('review_show', array('id' => $review->getId()));
+        }
+
+        return $this->render('review/new.html.twig',  array(
+            'review' => $review,
+            'form' => $form->createView(),
+        ));
     }
 
+    /**
+     * show review
+     *
+     * @Route("/{id}", name="review_show")
+     * @Method("GET")
+     */
+    public function showAction(Review $review)
+    {
+        $deleteForm = $this->createDeleteForm($review);
+
+        return $this->render('review/show.html.twig', array(
+            'review' => $review,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * edit review
+     *
+     * @Route("/{id}/edit", name="review_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Review $review)
+    {
+        $deleteForm = $this->createDeleteForm($review);
+        $editForm = $this->createForm('AppBundle\Form\ReviewType', $review);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('review_edit', array('id' => $review->getId()));
+        }
+
+        return $this->render('review/edit.html.twig', array(
+            'review' => $review,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a review entity.
+     *
+     * @Route("/{id}", name="review_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Review $review)
+    {
+        $form = $this->createDeleteForm($review);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($review);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('review_index');
+    }
+
+    /**
+     * Creates a form to delete a flight entity.
+     *
+     * @param Review $review The review entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Review $review)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('review_delete', array('id' => $review->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+            ;
+    }
 }
